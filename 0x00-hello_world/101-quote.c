d44ce05ae97349300de18117a807836051ded990
Mohau <matthewsmohau@gmail.com>
2022-09-08 20:20:45 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
